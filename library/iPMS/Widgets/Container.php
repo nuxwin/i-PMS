c71df2dc0b5d34f805357daaefe392bfa1328710
Laurent Declercq <l.declercq@nuxwin.com>
2011-03-20 13:38:47 +0100

    Added Iterator, countable interfaces and some other methods to the iPMS_Widgets_Container class

--- library/iPMS/Widgets/Container.php
+++ library/iPMS/Widgets/Container.php
@@ -32,7 +32,7 @@
  * @author      Laurent Declercq <l.declercq@nuxwin.com>
  * @version     1.0.0
  */
-class iPMS_Widgets_Container
+class iPMS_Widgets_Container implements Iterator, Countable
 {
 
     /**
@@ -73,6 +73,46 @@ class iPMS_Widgets_Container
     }
 
     /**
+     * Sorts the widget index according to widget order
+     *
+     * @return  void
+     */
+    protected function _sort()
+    {
+        if ($this->_dirtyIndex) {
+            $newIndex = array();
+            $index = 0;
+
+            /**
+             * @var $widget iPMS_Widget
+             */
+            foreach ($this->_widgets as $hash => $widget) {
+                $order = $widget->getOrder();
+                if ($order === null) {
+                    $newIndex[$hash] = $index;
+                    $index++;
+                } else {
+                    $newIndex[$hash] = $order;
+                }
+            }
+
+            asort($newIndex);
+            $this->_index = $newIndex;
+            $this->_dirtyIndex = false;
+        }
+    }
+
+    /**
+     * Notifies container that the order of widget are updated
+     *
+     * @return  void
+     */
+    public function notifyOrderUpdated()
+    {
+        $this->_dirtyIndex = true;
+    }
+
+    /**
      * Adds a widget to the container
      *
      * This method will inject the container as the given page's parent by
@@ -84,14 +124,6 @@ class iPMS_Widgets_Container
      */
     public function addWidget($widget)
     {
-        /*
-        if ($widget === $this) {
-            require_once 'Zend/Navigation/Exception.php';
-            throw new Zend_Navigation_Exception(
-                'A page cannot have itself as a parent');
-        }
-         */
-
         if (is_array($widget)) {
             require_once 'iPMS/Widget.php';
             $widget = iPMS_Widget::factory($widget);
@@ -113,8 +145,8 @@ class iPMS_Widgets_Container
         $this->_index[$hash] = $widget->getOrder(); // Todo method to be added in the iPMS_Widget abstract class
         $this->_dirtyIndex = true;
 
-        // inject self as page parent
-        //$page->setParent($this);
+        // inject self as widget parent
+        //$widget->setParent($this);
 
         return $this;
     }
@@ -139,5 +171,198 @@ class iPMS_Widgets_Container
         }
 
         return $this;
+    }
+
+    /**
+     * Sets widgets this container should have, removing existing widgets
+     *
+     * @param   array $widgets widgets to set
+     * @return  iPMS_Widget_Container fluent interface, returns self
+     */
+    public function setWidgets(array $widgets)
+    {
+        $this->removeWidgets();
+        return $this->addWidgets($widgets);
+    }
+
+    /**
+     * Returns widgets in the container
+     *
+     * @return  array array of iPMS_Widget instances
+     */
+    public function getWidgets()
+    {
+        return $this->_widgets;
+    }
+
+    /**
+     * Removes the given widget from the container
+     *
+     * @param   iPMS_Widget|int $widget widget to remove, either a widget instance or a specific widget order
+     * @return  bool whether the removal was successful
+     */
+    public function removeWidget($widget)
+    {
+        if ($widget instanceof iPMS_Widget) {
+            $hash = $widget->hashCode();
+        } elseif (is_int($widget)) {
+            $this->_sort();
+            if (!$hash = array_search($widget, $this->_index)) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        if (isset($this->_widgets[$hash])) {
+            unset($this->_widgets[$hash]);
+            unset($this->_index[$hash]);
+            $this->_dirtyIndex = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Removes all widget in container
+     *
+     * @return  iPMS_Widgets_Container fluent interface, returns self
+     */
+    public function removeWidgets()
+    {
+        $this->_widgets = array();
+        $this->_index = array();
+        return $this;
+    }
+
+    /**
+     * Returns true if container contains any widgets
+     *
+     * @return  bool whether container has any widgets
+     */
+    public function hasWidgets()
+    {
+        return count($this->_index) > 0;
+    }
+
+    /**
+     * Returns current widget
+     *
+     * Implements Iterator interface.
+     *
+     * @return  iPMS_Widget current widget or null
+     * @throws  iPMS_Widgets_Exception if the index is invalid
+     */
+    public function current()
+    {
+        $this->_sort();
+        current($this->_index);
+        $hash = key($this->_index);
+
+        if (isset($this->_widgets[$hash])) {
+            return $this->_widgets[$hash];
+        } else {
+            require_once 'iPMS/Widgets/Exception.php';
+            throw new iPMS_Widgets_Exception(
+                'Corruption detected in container; invalid key found in internal iterator'
+            );
+        }
+    }
+
+    /**
+     * Returns hash code of current widget
+     *
+     * Implements Iterator interface.
+     *
+     * @return  string hash code of current widget
+     */
+    public function key()
+    {
+        $this->_sort();
+        return key($this->_index);
+    }
+
+    /**
+     * Moves index pointer to next widget in the container
+     *
+     * Implements Iterator interface.
+     *
+     * @return  void
+     */
+    public function next()
+    {
+        $this->_sort();
+        next($this->_index);
+    }
+
+    /**
+     * Sets index pointer to first widget in the container
+     *
+     * Implements Iterator interface.
+     *
+     * @return  void
+     */
+    public function rewind()
+    {
+        $this->_sort();
+        reset($this->_index);
+    }
+
+    /**
+     * Checks if container index is valid
+     *
+     * Implements Iterator interface.
+     *
+     * @return  bool
+     */
+    public function valid()
+    {
+        $this->_sort();
+        return current($this->_index) !== false;
+    }
+
+    /**
+     * Proxy to hasWidgets()
+     *
+     * Implements Iterator interface.
+     *
+     * @return  bool whether container has any widgets
+     */
+    public function hasChildren()
+    {
+        return $this->hasWidgets();
+    }
+
+    /**
+     * Returns the child container.
+     *
+     * Implements Iterator interface.
+     *
+     * @return  iPMS_Widget null
+     */
+    public function getChildren()
+    {
+        $hash = key($this->_index);
+
+        if (isset($this->_widgets[$hash])) {
+            return $this->_widgets[$hash];
+        }
+
+        return null;
+    }
+
+    // Countable interface:
+
+    /**
+     * Returns number of widgets in container
+     *
+     * Implements Countable interface.
+     *
+     * @return  int number of widgets in the container
+     */
+    public function count()
+    {
+        return count($this->_index);
     }
 }
