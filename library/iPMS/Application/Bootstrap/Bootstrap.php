44d15cfbfa17ef423b4333fe62bb38096b9a3c6e
Laurent Declercq <l.declercq@nuxwin.com>
2011-05-21 08:49:56 +0200

    [Dependency Injection]  - Prototype

--- library/iPMS/Application/Bootstrap/Bootstrap.php
+++ library/iPMS/Application/Bootstrap/Bootstrap.php
@@ -27,13 +27,14 @@
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
  */
 
-use \iPMS\DependencyInjection\ContainerBuilder as serviceContainer;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * Bootstrap class for Dependency Injection concerns
  *
- * This class extends the Zend_Application_Bootstrap_Bootstrap to replace the default resources container by an that
- * implements the Symfony DI ContainerInterface interface.
+ * This class extends the Zend_Application_Bootstrap_Bootstrap to make it compatible
+ * with the Symfony dependency injection container. The DI container replaces the
+ * default resources container (Zend_Registry) to support dependency injection.
  *
  * @package     iPMS
  * @subpackage  Application
@@ -44,23 +45,117 @@ use \iPMS\DependencyInjection\ContainerBuilder as serviceContainer;
  */
 class iPMS_Application_Bootstrap_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-	/**
-	 * Retrieve resource container
-	 *
-	 * @return object
-	 */
-
 	/**
 	 * Returns service container
 	 * 
-	 * @return \iPMS\DependencyInjection\ContainerBuilder
+	 * @return Symfony\Component\DependencyInjection\ContainerBuilder
 	 */
 	public function getContainer()
 	{
 		if (null === $this->_container) {
-			$this->setContainer(new serviceContainer());
+			$this->setContainer(new ContainerBuilder());
 		}
 		return $this->_container;
 	}
+
+    /**
+     * Retrieve a resource from the container
+     *
+     * During bootstrap resource initialization, you may return a value. If
+     * you do, it will be stored in the {@link setContainer() container}.
+     * You can use this method to retrieve that value.
+     *
+     * If no value was returned, this will return a null value.
+     *
+     * @param  string $name
+     * @return null|mixed
+     */
+	public function getResource($name)
+	{
+        $resource  = strtolower($name);
+        $container = $this->getContainer();
+        if ($this->hasResource($resource)) {
+            //return $container->{$resource};
+	        return $container->get($resource);
+        }
+        return null;
+	}
+
+    /**
+     * Determine if a resource has been stored in the container
+     *
+     * During bootstrap resource initialization, you may return a value. If
+     * you do, it will be stored in the {@link setContainer() container}.
+     * You can use this method to determine if a value was stored.
+     *
+     * @param  string $name
+     * @return bool
+     */
+	public function hasResource($name)
+	{
+        $resource  = strtolower($name);
+        $container = $this->getContainer();
+        //return isset($container->{$resource});
+		return $container->has($resource);
+	}
+
+    /**
+     * Execute a resource
+     *
+     * Checks to see if the resource has already been run. If not, it searches
+     * first to see if a local method matches the resource, and executes that.
+     * If not, it checks to see if a plugin resource matches, and executes that
+     * if found.
+     *
+     * Finally, if not found, it throws an exception.
+     *
+     * @param  string $resource
+     * @return void
+     * @throws Zend_Application_Bootstrap_Exception When resource not found
+     */
+    protected function _executeResource($resource)
+    {
+        $resourceName = strtolower($resource);
+
+        if (in_array($resourceName, $this->_run)) {
+            return;
+        }
+
+        if (isset($this->_started[$resourceName]) && $this->_started[$resourceName]) {
+            throw new Zend_Application_Bootstrap_Exception('Circular resource dependency detected');
+        }
+
+        $classResources = $this->getClassResources();
+        if (array_key_exists($resourceName, $classResources)) {
+            $this->_started[$resourceName] = true;
+            $method = $classResources[$resourceName];
+            $return = $this->$method();
+            unset($this->_started[$resourceName]);
+            $this->_markRun($resourceName);
+
+            if (null !== $return) {
+                //$this->getContainer()->{$resourceName} = $return;
+	            $this->getContainer()->set($resourceName, $return);
+            }
+
+            return;
+        }
+
+        if ($this->hasPluginResource($resource)) {
+            $this->_started[$resourceName] = true;
+            $plugin = $this->getPluginResource($resource);
+            $return = $plugin->init();
+            unset($this->_started[$resourceName]);
+            $this->_markRun($resourceName);
+
+            if (null !== $return) {
+                //$this->getContainer()->{$resourceName} = $return;
+	            $this->getContainer()->set($resourceName, $return);
+            }
+
+            return;
+        }
+
+        throw new Zend_Application_Bootstrap_Exception('Resource matching "' . $resource . '" not found');
+    }
 }
